20601d8384ccc3850f643ad1545e65f87f5be2a2
Dacio Romero <dacioromero@gmail.com>
2019-03-20 16:07:55 -0700

    Remove lodash.bind and sinon-test dev dependencies, support CommonJS require without .default

--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,19 @@
 import sinon from 'sinon';
-import sinonTest from 'sinon-test';
-import bind from 'lodash.bind';
 import populate from '../src';
-
-const test = sinonTest(sinon);
 
 // https://github.com/vikpe/mongoose-middleware-test
 describe('Helper', () => {
-  it('should call this.populate, next', test(function helperTest() {
-    const model = { populate: this.spy() };
+  it('should call this.populate, next', () => {
+    const model = { populate: sinon.spy() };
 
     const field = 'test';
-    const boundHelper = bind(populate(field), model);
-    const next = this.spy();
+    const boundHelper = populate(field).bind(model);
+    const next = sinon.spy();
 
     boundHelper(next);
 
     sinon.assert.calledOnce(model.populate);
     sinon.assert.calledWith(model.populate, field);
     sinon.assert.calledOnce(next);
-  }));
+  });
 });
