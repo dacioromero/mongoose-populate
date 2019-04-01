01e9db92c9a95c017682ce326adc37d5c5d260ae
Dacio Romero <dacioromero@gmail.com>
2019-04-01 15:04:05 -0700

    Use Jest over Mocha and Sinon, switch GitLab CI to Snippet, use "files" field in package.json

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,18 @@
-import sinon from 'sinon';
 import populate from '../src';
 
 // https://github.com/vikpe/mongoose-middleware-test
 describe('Helper', () => {
   it('should call this.populate, next', () => {
-    const model = { populate: sinon.spy() };
+    const model = { populate: jest.fn() };
 
     const field = 'test';
     const boundHelper = populate(field).bind(model);
-    const next = sinon.spy();
+    const next = jest.fn();
 
     boundHelper(next);
 
-    sinon.assert.calledOnce(model.populate);
-    sinon.assert.calledWith(model.populate, field);
-    sinon.assert.calledOnce(next);
+    expect(model.populate.mock.calls.length).toBe(1);
+    expect(model.populate.mock.calls[0][0]).toBe(field);
+    expect(model.populate.mock.calls.length).toBe(1);
   });
 });
