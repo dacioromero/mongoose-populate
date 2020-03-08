40597bd0b0ab1e170a4b7d31e4e4bc6e536899f3
Dacio Romero <dacioromero@gmail.com>
2020-03-08 03:02:21 -0700

    Apply formatting, fix testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,18 @@
-import { createPopulateHook } from '.';
+import { createPopulateHook } from '.'
 
 // https://github.com/vikpe/mongoose-middleware-test
 describe('populate hook', () => {
   it('should call this.populate and next', () => {
-    const query = { populate: jest.fn() };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const query: any = { populate: jest.fn() }
 
-    const field = 'test';
-    const next = jest.fn();
+    const field = 'test'
+    const next = jest.fn()
 
     createPopulateHook(field).call(query, next, [])
 
-    expect(query.populate.mock.calls.length).toBe(1);
-    expect(query.populate.mock.calls[0][0]).toBe(field);
-    expect(next.mock.calls.length).toBe(1);
-  });
-});
+    expect(query.populate.mock.calls.length).toBe(1)
+    expect(query.populate.mock.calls[0][0]).toBe(field)
+    expect(next.mock.calls.length).toBe(1)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Query, HookSyncCallback, HookNextFunction } from "mongoose";
+import { Query, HookSyncCallback, HookNextFunction } from 'mongoose'
 
 // https://stackoverflow.com/a/30052105/10336544
-export function createPopulateHook <T extends Query<unknown>> (field: string): HookSyncCallback<T> {
+export function createPopulateHook<T extends Query<unknown>>(
+  field: string
+): HookSyncCallback<T> {
   return function populateHook(this: T, next: HookNextFunction): void {
-    this.populate(field);
-    next();
-  };
-};
+    this.populate(field)
+    next()
+  }
+}
