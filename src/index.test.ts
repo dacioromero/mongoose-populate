672410f7b29681f1019c1aed38e49e4029030fca
Dacio Romero <dacioromero@gmail.com>
2020-03-08 19:17:58 -0700

    foo

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,13 +3,13 @@ import { createPopulateHook } from '.'
 // https://github.com/vikpe/mongoose-middleware-test
 describe('populate hook', () => {
   it('should call this.populate and next', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const query: any = { populate: jest.fn() }
+    const query = { populate: jest.fn() }
 
     const field = 'test'
     const next = jest.fn()
 
-    createPopulateHook(field).call(query, next, [])
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    createPopulateHook(field).call(query as any, next, [])
 
     expect(query.populate.mock.calls.length).toBe(1)
     expect(query.populate.mock.calls[0][0]).toBe(field)
