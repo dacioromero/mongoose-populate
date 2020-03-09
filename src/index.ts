e58c017a281a384e0fccc9cac797bd73298a0218
Dacio Romero <dacioromero@gmail.com>
2020-03-08 21:02:40 -0700

    chore: use packaged eslint and prettier configs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// https://github.com/benmosher/eslint-plugin-import/issues/1618
+// eslint-disable-next-line import/no-unresolved
 import { Query, HookSyncCallback, HookNextFunction } from 'mongoose'
 
 // https://stackoverflow.com/a/30052105/10336544
